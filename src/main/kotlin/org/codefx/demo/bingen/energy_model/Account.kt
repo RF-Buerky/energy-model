cdd132a6cda96cbfd9d39664c8f3040e88132b10
R_Buerky <roman.f.buerky@th-bingen.de>
2017-07-01 12:51:14 +0200

    improved the following Funktion: fun deposit(amount: Int): Int {...}

--- src/main/kotlin/org/codefx/demo/bingen/energy_model/Account.kt
+++ src/main/kotlin/org/codefx/demo/bingen/energy_model/Account.kt
@@ -3,8 +3,35 @@ package org.codefx.demo.bingen.energy_model
 class Account(var balance: Int, var limit: Int) {
 
     fun deposit(amount: Int): Int {
+        // _________________________________________________________________________________
+        // The following lines show the original code commented out. There have been done mistakes
+        // py purpose to make the test in the test file "AccountTest" fail. The code had to
+        // be improved in order to make the tests pass. The improved code is added after the
+        // original code.
+        // _________________________________________________________________________________
+        /*
         balance += amount
         return amount
+        */
+
+        // _________________________________________________________________________________
+        // The following lines show the improved code. The improvements have been done by
+        // viewing the functions in the test file "AccountTest".
+        // _________________________________________________________________________________
+
+        // Referring to the function "negativeDepositDoesNotChangeBalance()", there should be no
+        // incident on the balance if a negative amount is about to get deposit. This is a plausible
+        // way as it is not possible to deposit a negative amount of money onto your bank account.
+        // The equivalent way would be to withdraw the (positive) amount of money. So this is the way
+        // we are going here, also.
+        if (amount < 0){
+            val deposited : Int = 0
+            return deposited
+        } else {
+            balance = balance + amount
+            return balance
+        }
+
     }
 
     fun withdraw(amount: Int): Int {
