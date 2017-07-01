8155a4a7233778c42b478c008acea2ded826d3cb
R_Buerky <roman.f.buerky@th-bingen.de>
2017-07-01 13:42:56 +0200

    improved the other two functions

--- src/main/kotlin/org/codefx/demo/bingen/energy_model/Account.kt
+++ src/main/kotlin/org/codefx/demo/bingen/energy_model/Account.kt
@@ -2,36 +2,17 @@ package org.codefx.demo.bingen.energy_model
 
 class Account(var balance: Int, var limit: Int) {
 
+
+    // _________________________________________________________________________________
+    // The following lines show the original code commented out. There have been done mistakes
+    // py purpose to make the tests in the test file "AccountTest" fail. The code had to
+    // be improved in order to make the tests pass. The improved code is added after the
+    // original code.
+    // _________________________________________________________________________________
+    /*
     fun deposit(amount: Int): Int {
-        // _________________________________________________________________________________
-        // The following lines show the original code commented out. There have been done mistakes
-        // py purpose to make the test in the test file "AccountTest" fail. The code had to
-        // be improved in order to make the tests pass. The improved code is added after the
-        // original code.
-        // _________________________________________________________________________________
-        /*
         balance += amount
         return amount
-        */
-
-        // _________________________________________________________________________________
-        // The following lines show the improved code. The improvements have been done by
-        // viewing the functions in the test file "AccountTest".
-        // _________________________________________________________________________________
-
-        // Referring to the function "negativeDepositDoesNotChangeBalance()", there should be no
-        // incident on the balance if a negative amount is about to get deposit. This is a plausible
-        // way as it is not possible to deposit a negative amount of money onto your bank account.
-        // The equivalent way would be to withdraw the (positive) amount of money. So this is the way
-        // we are going here, also.
-        if (amount < 0){
-            val deposited : Int = 0
-            return deposited
-        } else {
-            balance = balance + amount
-            return balance
-        }
-
     }
 
     fun withdraw(amount: Int): Int {
@@ -44,6 +25,60 @@ class Account(var balance: Int, var limit: Int) {
             return 0
         }
     }
+
+    fun currentBalance(): Int {
+        return balance
+    }
+    */
+
+    // _________________________________________________________________________________
+    // The following lines show the improved code. The improvements have been done by
+    // viewing the functions in the test file "AccountTest".
+    // _________________________________________________________________________________
+
+    // Referring to the function "negativeDepositDoesNotChangeBalance()", there should be no
+    // incident on the balance if a negative amount is about to get deposit. This is a plausible
+    // way as it is not possible to deposit a negative amount of money onto your bank account.
+    // The equivalent way would be to withdraw the (positive) amount of money. So this is the way
+    // we are going here, also.
+    fun deposit(amount: Int): Int {
+        var deposited_amount : Int = 0
+        if (amount < 0){
+            deposited_amount = 0
+            balance = balance
+        } else {
+            deposited_amount = amount
+            balance = balance + amount
+        }
+        return deposited_amount
+    }
+
+    // _________________________________________________________________________________
+    // Referring to the function "negativeWithdrawalDoesNotChangeBalance()", there should be no
+    // incident on the balance if a negative amount is about to get withdrawn. This is a plausible
+    // way as it is not possible to withdraw a negative amount of money by your bank account.
+    // The equivalent way would be to deposit the (positive) amount of money. So this is the way
+    // we are going here, also.
+    fun withdraw(amount: Int): Int {
+        var withdrawed_amount : Int = 0
+
+        if (amount < 0) {
+            withdrawed_amount = 0
+            balance = balance
+        } else if (balance >= amount) {
+                // balance suffices, pay out the money
+            withdrawed_amount = amount
+            balance = balance - amount
+        } else if (balance < amount && balance-amount>limit) {
+            withdrawed_amount = amount
+            balance = balance - amount
+        } else {
+            withdrawed_amount = 0
+            balance = balance
+        }
+        return withdrawed_amount
+        }
+
 
     fun currentBalance(): Int {
         return balance
